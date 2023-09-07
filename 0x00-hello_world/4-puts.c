dcbced40753b89c9b57fca43a4849e384857abcd
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-07 07:13:43 -0700

    check 4-puts errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+/**
+ * Main: entry to the program
+ * return:should return 0 if success
+*/ 
 int main(void)	
+
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);	    
