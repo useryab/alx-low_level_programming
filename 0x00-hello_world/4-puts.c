1310718efcc81bc00435ade01a3e9e55a3e72b3c
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-07 07:17:34 -0700

    check 4-puts errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * Main: entry to the program
- * return:should return 0 if success
-*/ 
-int main(void)	
-
+ * main:entry to the program
+ * Return: 0 meaning success
+ */
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);	    
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
