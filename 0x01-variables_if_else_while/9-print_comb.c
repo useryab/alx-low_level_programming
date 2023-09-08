c2d7472753ee1c9852e1ba8dd07d5ee05cc038f7
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-08 09:50:42 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,19 @@
 */
 int main(void)
 {
-	int c = 0;
+	int num;
 
-	while (c < 10)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(48 + c);
-		if (c != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		c++;
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
