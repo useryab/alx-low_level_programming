b3e4de4aefef6b73ab8712ba297b0d0ed8f3aab6
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-08 01:09:25 -0700

    5 documentation error fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - Entry point 
-* Return: Always 0 (Success)
-*/
+ * main - main is the entry to the program
+ * Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
