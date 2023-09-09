a1a09e77776903b790b1d2c646075d06038ca698
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-08 21:35:51 -0700

    error fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main Entry 
- * Return: 0 
+ * main - main Entry
+ * Return: 0
 */
 int main(void)
 {
-	int x;
-	char b;
-
-	for (x = 48; x < 58; x++)
-	{
-		putchar(x);
-	}
-	for (b = 'a'; b <= 'f'; b++)
-	{
-		putchar(b);
-	}
-	putchar('\n');
-	return (0);
+int a;
+char b;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+for (b = 'a'; b <= 'f'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
 }
