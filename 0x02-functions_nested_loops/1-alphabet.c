c285e762a0c408eb62554ea940d153df84e3bb4d
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-13 10:22:14 -0700

    1 fix

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry
+ * return: 0
+*/
+void print_alphabet(void)
+{
+char chr = 'a';
+while (chr <= 'z')
+{
+_putchar(chr);
+chr++;
+}
+_putchar('\n');
+}
