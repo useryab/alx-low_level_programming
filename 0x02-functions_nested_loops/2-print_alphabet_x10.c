7615a77b98b032c976cf6972d7be2b2ec1f32ecb
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-13 11:06:22 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+{
+char chr;
+int = 0;
+while (i < 10)
+{
+char chr = 'a';
+while (chr <= 'z')
+{
+_putchar(chr);
+chr++
+}
+_putchar('\n');
+i++
+}
+}
