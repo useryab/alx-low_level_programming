b955747192057d4e8f403bd8a792641565191051
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-15 10:30:07 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks character
+ * @chr: character to be checked
+ * Return: 1 if chr is uppercase, 0 otherwise
+ */
+int _isupper(int chr)
+{
+	return (chr >= 'A' && chr <= 'Z');
+}
+
