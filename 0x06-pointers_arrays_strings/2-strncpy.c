7412de991ce1aebf52a6a347958043ef84139c52
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-20 13:56:08 -0700

    1-3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings 
+ * @dest: input
+ * @src: input 
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int j;
+
+    j = 0;
+    while (j<n && src[j] != '\0')
+    {
+        dest[j] = src[j];
+        j++;
+    }
+    while (j<n)
+    {
+        dest[j] = '\0';
+        j++;
+    }
+    return(dest);
+    
+}
