c60dba6a41e232afbf3a97bacfecd8da5ea314e6
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-21 12:47:59 -0700

    fix2+files added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_number - print numbers chars
  * @n: integer params
  * Return: 0
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i;
-    int t;
-    for (i = 0; i < n--; i++)
-    {
-        t = a[i];
-        a[i] = a[n];
-        a[n] = t;
-    }
-    
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
 }
