52b739e5e83e11cf48aaefbc70a6e719bc0a5420
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-09-25 16:07:40 -0700

    error fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _strstr - Write a function that locates a substring.
- * @haystack: This is the input string
- * @needle: This is a find to locate in the string
- * Return: a pointer to the beginning of the located substring, or NULL if the
- * substring is not found.
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
 {
