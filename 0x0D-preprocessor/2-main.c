7557a711ebc982bff2fb77bc491a69d186801ae8
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-10-09 03:51:53 -0700

    error fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,10 @@
 #include<stdio.h>
 /**
  * main - Prints the name of the file
- *
  * Return: Always 0
  */
 int main(void)
 {
-/**
- * __FILE__ - is a standard predifined macro in the stdio library that
- * expands to the name of the current input file
- * in the form of a C string constant
-*/
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
