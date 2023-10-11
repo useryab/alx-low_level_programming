27586eb48482560c98090bfa0634627cb47920a3
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-10-11 03:53:53 -0700

    Task files initialized + Task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name:name to be printed
+ * @f:pointer to function that prints name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+	else
+		return;
+}
