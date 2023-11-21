d9c5aff69c7c6abbef7f8ee01adca87a1d5870d5
useryab <yabsiratekalegn53@gmail.com>
2023-11-21 09:31:17 -0800

    error check done and fixed

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if alphabetic
+ * @c: character to be checked
+ * Return: 1 if character is letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
