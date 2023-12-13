c3421e33ce8ee37200c8655ed8a9bf4a2441c442
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2023-12-13 10:10:29 -0800

    error fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * print_listint - function to print all elements
- * @h: listint_t type of node struct
- * Return: Always successful
+ * listint_len - This function return to the number of nudes
+ * @h: This is my structure entry
+ * Return: This return to the number the nodes
+ *
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-	unsigned int count;
+	unsigned int count = 0;
 
-	count = 0;
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		count += 1;
 		h = h->next;
-		count++;
 	}
 	return (count);
 }
